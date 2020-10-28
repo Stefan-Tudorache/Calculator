d6f7493df04ff9a3b5870567ecfed58361d3ac9a
Stefan <stefantudorache90@yahoo.com>
2020-10-28 16:24:41 +0200

    finished-small-calculator

--- main.js
+++ main.js
@@ -1,59 +1,55 @@
-
-const calculate = () => {
-
-  const inputOne = document.querySelector("#firstNumber").value;
-  const inputTwo = document.querySelector("#secondNumber").value;
-
-  const numberOne = parseInt(inputOne);
-  const numberTwo = parseInt(inputTwo);
-
-  const operator = document.querySelector('#operator').value;
-  const pTag = document.querySelector("#result");
-
-
-  if (isNaN(numberOne) || isNaN(numberTwo)) {
-    pTag.innerHTML = "You Broke it !";
-    alert("You Broke It !!! Please enter a number or a correct operator in the input boxes.")
-
-  } else if (operator === "+") {
-    pTag.innerHTML = numberOne + numberTwo;
-
-  } else if (operator === "-") {
-    pTag.innerHTML = numberOne - numberTwo;
-
-  } else if (operator === "*") {
-    pTag.innerHTML = numberOne * numberTwo;
-
-  } else if (operator === "/") {
-    pTag.innerHTML = numberOne / numberTwo;
-
-  } else if (operator === "**") {
-    pTag.innerHTML = numberOne ** numberTwo;
-
-  } else {
-    pTag.innerHTML = "You Broke it !";
-    alert("You Broke It !!! Please enter a number or a correct operator in the input boxes.")
-  }
-}
-
-
-
-const reset = () => {
-
+// Variables
   const inputOne = document.querySelector("#firstNumber");
   const inputTwo = document.querySelector("#secondNumber");
 
   const operator = document.querySelector('#operator');
   const pTag = document.querySelector("#result");
 
-  inputOne.value = " ";
-  inputTwo.value = " ";
-  operator.value = " ";
-  pTag.innerHTML = "Let's do it again!"
+//Calculate function
+const calculate = () => {
+
+  const numberOne = parseInt(inputOne.value);
+  const numberTwo = parseInt(inputTwo.value);
+
+  if (isNaN(numberOne) || isNaN(numberTwo)) {
+    pTag.innerHTML = "You Broke it !";
+    alert("You Broke It !!! Please enter a number or a correct operator in the input boxes.");
+
+  } else if (operator.value === "+") {
+    pTag.innerHTML = numberOne + numberTwo;
+
+  } else if (operator.value === "-") {
+    pTag.innerHTML = numberOne - numberTwo;
+
+  } else if (operator.value === "*") {
+    pTag.innerHTML = numberOne * numberTwo;
+
+  } else if (operator.value === "/") {
+    pTag.innerHTML = numberOne / numberTwo;
+
+  } else if (operator.value === "**") {
+    pTag.innerHTML = numberOne ** numberTwo;
+    
+  }  else {
+    pTag.innerHTML = "You Broke it !";
+    alert("You Broke It !!! Please enter a number or a correct operator in the input boxes.");   
+  }
 }
 
+//Calling function on click event
 document.querySelector("#calculate").addEventListener("click", calculate);
 
+//Reset function for inputs and result
+const reset = () => {
+
+  inputOne.value = "";
+  inputTwo.value = "";
+  operator.value = "";
+  pTag.innerHTML = "Let's do it again!"
+
+}
+
+//Calling function on click event
 document.querySelector("#reset").addEventListener("click", reset);
 
 
